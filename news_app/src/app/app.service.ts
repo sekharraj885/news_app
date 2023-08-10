4d9014dec66f7f42761f93fef6c12c96105a42b7
unknown <rajasekharmandireddy.gmail.com>
2023-08-10 16:19:27 +0530

    API key added

--- news_app/src/app/app.service.ts
+++ news_app/src/app/app.service.ts
@@ -13,8 +13,8 @@ export class AppService {
 
 
   BASE_NEWS_API = 'https://newsapi.org/v2/'
-  // API_KEY= 'a4904d5e0218459184d699dccec9b5e1'
-  API_KEY= 'a4904d5e0218459184d6'
+  API_KEY= 'a4904d5e0218459184d699dccec9b5e1'
+  // API_KEY= 'a4904d5e0218459184d6'
 
   getNews(searchPhrase:String,pagesize:number,page:number){    
     const NEWS_API = this.BASE_NEWS_API+searchPhrase+'&pageSize='+pagesize+'&page='+page+'&apiKey='+this.API_KEY
